a9dd560baa000be55502a49999de31b905ed2c3c
Nulduvu <eliot.kosinski@gmail.com>
2026-06-25 20:17:56 +0200

    finish the projects.html.html file with the css (i'm less happy)

--- eliot.js
+++
@@ -1,9 +0,0 @@
-// Compteur de clics personnalisé
-let nbLikes = 0;
-const boutonLike = document.getElementById("my_like_button");
-if (boutonLike) {
-boutonLike.addEventListener("click", () => {
-    nbLikes = nbLikes + 1;
-    boutonLike.textContent = "❤ J'aime (" + nbLikes + ")";
-});
-}
